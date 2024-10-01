b1fe4cf0f001468ab6e22edc4c97392044c4c86d
wishabc <aswq22013@gmail.com>
2024-10-01 15:43:01 -0700

    Refactor runhotspot_with_peaks.bash script to include additional scripts for better peak finding and library generation

--- bin/runhotspot_with_peaks.bash
+++ bin/runhotspot_with_peaks.bash
@@ -116,9 +116,6 @@ _CLEAN_ = T
 
 _PKFIND_BIN_ = $HOTSPOT_DISTR/hotspot-deploy/bin/wavePeaks
 
-wavelets=$HOTSPOT_DISTR/hotspot-deploy/bin/wavelets
-which wavelets
-
 ## Peak-finding smoothing level. If the resolution of the input file
 ## is x, then the results are smoothed out to a scale of (2^level)*x.
 _PKFIND_SMTH_LVL_ = 3
@@ -171,6 +168,8 @@ scripts="
     \$tokenFile \
     \$scripts
 
+wavelets=$HOTSPOT_DISTR/hotspot-deploy/bin/wavelets
+which wavelets
 for script in \$scripts
 do
     ./\$(basename \$script).tok
